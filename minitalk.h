f3258fef6e8407811b2b3e52a33dbe2caf9f88ff
Nonojim <nolan.pederencino@gmail.com>
2025-02-03 21:50:50 +0100

    first commit

---
+++ minitalk.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: npederen <npederen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/03 17:36:50 by npederen          #+#    #+#             */
+/*   Updated: 2025/02/03 17:36:50 by npederen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <signal.h>
+# include "printf/libft/libft.h"
+# include "printf/ft_printf.h"
+
+#endif
